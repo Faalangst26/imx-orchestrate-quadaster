f1ad7825154a85f567f40e0a8a917653318cdbf3
berne <s1156738@student.windesheim.nl>
2023-12-11 14:36:47 +0100

    Het begin van het project

---
+++ source-rest/src/main/java/nl/geostandaarden/imx/orchestrate/souce/rest/Main.java
@@ -0,0 +1,9 @@
+package nl.geostandaarden.imx.orchestrate.souce.rest;
+
+//Het begin van iets moois
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
