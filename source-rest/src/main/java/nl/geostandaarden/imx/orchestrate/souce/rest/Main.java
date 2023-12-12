35617de399b1a50d178de4181ae2746e31b1caaf
berne <s1156738@student.windesheim.nl>
2023-12-12 11:10:37 +0100

    Bestanden toegevoegd voor de basis van de REST adapter

--- source-rest/src/main/java/nl/geostandaarden/imx/orchestrate/souce/rest/Main.java
+++
@@ -1,9 +0,0 @@
-package nl.geostandaarden.imx.orchestrate.souce.rest;
-
-//Het begin van iets moois
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
