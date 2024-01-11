41c179f9da55c86c9d3418c40ffcd560c1747a87
Berne Gerrits <s1156738@student.windesheim.nl>
2024-01-11 14:33:37 +0000

    maven:3.9.6-eclipse-temurin-17-focal

--- .space.kts
+++ .space.kts
@@ -1,5 +1,5 @@
 job("Build and run tests") {
-    container(displayName = "Run mvn install", image = "maven:latest") {
+    container(displayName = "Run mvn install", image = "maven:3.9.6-eclipse-temurin-17-focal") {
         shellScript {
             content = """
 	            mvn clean install
