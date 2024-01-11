c6f6efeb688ec717589fb89d3389005ef3025feb
Berne Gerrits <s1156738@student.windesheim.nl>
2024-01-11 14:27:07 +0000

    Update CI Again Again

--- .space.kts
+++ .space.kts
@@ -1,5 +1,5 @@
 job("Build and run tests") {
-    container(displayName = "Run mvn install", image = "maven:3.3-jdk-17") {
+    container(displayName = "Run mvn install", image = "eclipse-temurin:17-jdk-focal") {
         shellScript {
             content = """
 	            mvn clean install
