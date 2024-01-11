74a0e8b04762fceab3a91d2467dc610303e6a407
Berne Gerrits <s1156738@student.windesheim.nl>
2024-01-11 14:12:34 +0000

    Update CI

--- .space.kts
+++ .space.kts
@@ -1,9 +1,14 @@
 job("Build and run tests") {
     container(displayName = "Run mvn install", image = "maven:latest") {
-        shellScript {
-            content = """
-	            mvn -B package --file pom.xml
-            """
-        }
+          steps {
+            script {
+                name = "Set up JDK 17"
+                scriptContent = "java { version = \"17\" }"
+            }
+
+            script {
+                name = "Build with Maven"
+                scriptContent = "mvn -B package --file pom.xml"
+            }
     }
 }
